59d1009273832695f88570d17844426fbeffab20
Theo Fanet <theo.fanet@gmail.com>
2017-06-02 22:44:59 +0200

    Update Updates.php

--- Module/Core/Cron/Updates.php
+++ Module/Core/Cron/Updates.php
@@ -11,34 +11,38 @@
 
         public function checkUpdates(){
             // First check core update
-            $rest = new TFW_Rest(TFW_Core::getUpdaterUrl());
+            $url = TFW_Core::getUpdaterUrl();
+            
+            if($url){
+                $rest = new TFW_Rest($url);
 
-            $rest
-                ->addHeader("Authorization", "Basic dGVzdGVyOnRlc3Rpc2xpZmU=")
-                ->addHeader("Api-Key", "5lf93rEm7baVAw026481")
-                ->addHeader("Content-Type", "application/json");
+                $rest
+                    ->addHeader("Authorization", "Basic dGVzdGVyOnRlc3Rpc2xpZmU=")
+                    ->addHeader("Api-Key", "5lf93rEm7baVAw026481")
+                    ->addHeader("Content-Type", "application/json");
 
-            $coreVersion = $rest->get("/info/core/version");
-            if($coreVersion){
-                if(version_compare(TFW_Core::getTFWVersion(), $coreVersion["version"])){
-                    TFW_Registry::setSetting("core.update", $coreVersion["id"]);
-                    TFW_Registry::setSetting("core.update.version", $coreVersion["version"]);
-                    TFW_Registry::setSetting("core.update.description", $coreVersion["description"]);
+                $coreVersion = $rest->get("/info/core/version");
+                if($coreVersion){
+                    if(version_compare(TFW_Core::getTFWVersion(), $coreVersion["version"])){
+                        TFW_Registry::setSetting("core.update", $coreVersion["id"]);
+                        TFW_Registry::setSetting("core.update.version", $coreVersion["version"]);
+                        TFW_Registry::setSetting("core.update.description", $coreVersion["description"]);
+                    }
                 }
-            }
 
-            $modules = TFW_Registry::getRegistered("module");
-            /**
-             * @var TFW_Module $module
-             */
-            foreach($modules as $module){
-                $module_version = $rest->post("/info/module/version", [$module->getKey()]);
+                $modules = TFW_Registry::getRegistered("module");
+                /**
+                 * @var TFW_Module $module
+                 */
+                foreach($modules as $module){
+                    $module_version = $rest->post("/info/module/version", [$module->getKey()]);
 
-                if($module_version && isset($module_version[$module->getKey()])){
-                    if(version_compare($module->getVersion(), $module_version[$module->getKey()]['version'])){
-                        TFW_Registry::setSetting("module.update.".$module->getKey(), $module_version[$module->getKey()]["id"]);
-                        TFW_Registry::setSetting("module.update.version.".$module->getKey(), $module_version[$module->getKey()]["version"]);
-                        TFW_Registry::setSetting("module.update.description.".$module->getKey(), $module_version[$module->getKey()]["description"]);
+                    if($module_version && isset($module_version[$module->getKey()])){
+                        if(version_compare($module->getVersion(), $module_version[$module->getKey()]['version'])){
+                            TFW_Registry::setSetting("module.update.".$module->getKey(), $module_version[$module->getKey()]["id"]);
+                            TFW_Registry::setSetting("module.update.version.".$module->getKey(), $module_version[$module->getKey()]["version"]);
+                            TFW_Registry::setSetting("module.update.description.".$module->getKey(), $module_version[$module->getKey()]["description"]);
+                        }
                     }
                 }
             }
